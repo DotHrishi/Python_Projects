d69e87b887e5b01f7b02a9ec8e7991f27ad3f523
DotHrishi <kalihrishikesh@gmail.com>
2024-09-01 16:04:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+participants=[]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def add_participant(name,bid):
+  new_participant={}
+  new_participant[name]=bid
+  participants.append(new_participant)
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text,shift_amount):
-    for letter in plain_text:
-        cipher_text=""
-        position=alphabet.index(letter)
-        new_position=position+shift_amount
-        new_letter=alphabet[new_position]
-        cipher_text+=new_letter
-    print(f"The encoded text is {cipher_text}")
-    
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+print("Welcome to the secret auction program.")
+name=input("What is your name?: ")
+bid=int(input("What's your bid?: $"))
+add_participant(name,bid)
+other_bidders=input("Are there any other bidders?")
+while other_bidders=="yes":
+  clear()
+  name=input("What is your name?: ")
+  bid=int(input("What's your bid?: $"))
+  add_participant(name,bid)
+  other_bidders=input("Are there any other bidders?")
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+max_bid=0
+for participant in participants:
+  for key in participant:
+    if participant[key]>max_bid:
+      max_bid=participant[key]
+      winner=key
+print(f"The winner is {winner} with a bid of ${max_bid}")
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(plain_text=text,shift_amount=shift)
+  
