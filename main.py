cfe699be3cdc8d77fe1b2047ed331560c61e9b2f
DotHrishi <kalihrishikesh@gmail.com>
2024-09-10 13:22:03 +0530

    Add files via upload
    Snake_game using python

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
-from replit import clear
-#HINT: You can call clear() to clear the output in the console.
-participants=[]
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
-def add_participant(name,bid):
-  new_participant={}
-  new_participant[name]=bid
-  participants.append(new_participant)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
-print("Welcome to the secret auction program.")
-name=input("What is your name?: ")
-bid=int(input("What's your bid?: $"))
-add_participant(name,bid)
-other_bidders=input("Are there any other bidders?")
-while other_bidders=="yes":
-  clear()
-  name=input("What is your name?: ")
-  bid=int(input("What's your bid?: $"))
-  add_participant(name,bid)
-  other_bidders=input("Are there any other bidders?")
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-max_bid=0
-for participant in participants:
-  for key in participant:
-    if participant[key]>max_bid:
-      max_bid=participant[key]
-      winner=key
-print(f"The winner is {winner} with a bid of ${max_bid}")
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-  
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
